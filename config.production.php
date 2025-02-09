1fd6e44f1b99748dd3d8b6aefe54a40e6b197c32
Mohammed Seyam <seyam.ms@gmail.com>
2025-02-09 17:08:25 +0300

    update base url and build for production

--- config.production.php
+++ config.production.php
@@ -3,7 +3,7 @@
 return [
 
     'production' => true,
-    'baseUrl' => $baseUrl = 'https://ic-ncnp.com',
+    'baseUrl' => $baseUrl = 'https://seyamms.github.io/M_incubator',
     'asset' => fn($page, $source) => $baseUrl . str($source)->start('/assets/'),
     'route' => fn($page, $source) => $baseUrl . str($source)->start('/'),
 
